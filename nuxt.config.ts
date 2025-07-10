e470c5ab4f532529497ae761f454746e4e8d1eb0
harvey_xiang <harvey_xiang@163.com>
2025-07-10 10:25:49 +0800

    feat: docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 import yaml from '@rollup/plugin-yaml'
 
+const armsScript = process.env.NODE_ENV === 'production'
+  ? [{ innerHTML: `var _czc = _czc || [];
+        (function () {
+          var um = document.createElement("script");
+          um.src = "https://v1.cnzz.com/z.js?id=1281423419&async=1";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(um, s);
+        })();`,
+    type: 'text/javascript' }]
+  : []
+
 const config = {
+  app: {
+    head: {
+      script: [
+        ...armsScript
+      ]
+    }
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
